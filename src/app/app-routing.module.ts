9cf11e05ca1b7aaf914aa9392c089fd5f82e3f69
keranis <mahdihadjsassi@fsb.u-carthage.tn>
2024-03-06 12:01:47 +0100

    init interfaces

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,40 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+// import { AuthGuard } from './guards/auth.guard';
+// import { PublicGuard } from './guards/public.guard';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: 'welcome', // TODO: Set this to ''
+    pathMatch: 'full'
   },
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    loadChildren: () => import('./pages/secure/secure.module').then(m => m.SecureModule),
+    // canActivate: [AuthGuard] // Secure all child pages
+  },
+  {
+    path: 'welcome',
+    loadChildren: () => import('./pages/public/welcome/welcome.module').then(m => m.WelcomePageModule),
+    // canActivate: [PublicGuard] // Prevent for signed in users
+  },
+  {
+    path: 'signin',
+    loadChildren: () => import('./pages/public/signin/signin.module').then(m => m.SigninPageModule),
+    // canActivate: [PublicGuard] // Prevent for signed in users
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./pages/public/signup/signup.module').then(m => m.SignupPageModule),
+    // canActivate: [PublicGuard] // Prevent for signed in users
+  },
+  {
+    path: 'password-reset',
+    loadChildren: () => import('./pages/public/password-reset/password-reset.module').then( m => m.PasswordResetPageModule),
+    // canActivate: [PublicGuard] // Prevent for signed in users
   },
 ];
-
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,25 @@ import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+// ReactiveForms
+import { ReactiveFormsModule } from '@angular/forms';
+
+// NgCharts
+// import { NgChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    ReactiveFormsModule,
+    AppRoutingModule,
+    // NgChartsModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
